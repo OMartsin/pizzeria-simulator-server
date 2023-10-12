84d4d1ad21049a1bbf199bf3f831aaa760e21216
OMartsin <oleksandrmartsin@gmail.com>
2023-10-12 13:05:23 +0300

    [feat] init spring project

---
+++ src/main/java/com/example/pizzeria/PizzeriaApplication.java
@@ -0,0 +1,21 @@
+package com.example.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaApplication.class, args);
+	}
+
+	@GetMapping()
+	public String sayHello() {
+		return "Hello world!";
+	}
+
+}
