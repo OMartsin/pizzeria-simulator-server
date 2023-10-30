cae67b30b85df87a2f89e04bd2d7c6ac4c8a3547
OMartsin <oleksandrmartsin@gmail.com>
2023-10-30 16:36:52 +0200

    [fix] fix file import problem

--- src/main/java/com/example/pizzeria/PizzaMenuReader.java
+++ src/main/java/com/example/pizzeria/PizzaMenuReader.java
@@ -5,21 +5,20 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.io.InputStream;
 import java.util.List;
 
 @Component
 public class PizzaMenuReader {
-    private final static String filePath = Paths.get("src", "main",
-            "resources", "pizza-menu.json").toString();
-
     public List<Recipe> getAllRecipes() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        List<Recipe> recipes;
-        recipes = objectMapper.readValue(new File(filePath), new TypeReference<>() {});
-        return recipes;
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("pizza-menu.json");
+
+        if (inputStream == null) {
+            throw new IOException("Could not find pizza-menu.json resource");
+        }
+
+        return objectMapper.readValue(inputStream, new TypeReference<>() {});
     }
 }
