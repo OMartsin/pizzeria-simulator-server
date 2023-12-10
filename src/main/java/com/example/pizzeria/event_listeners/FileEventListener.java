b13a9f2ff7e4a9cfad65207b547c795560c5de62
styanr <chaus.oleh@gmail.com>
2023-12-10 14:29:45 +0200

    [feat] add log4j2 dependency and file logging

--- src/main/java/com/example/pizzeria/event_listeners/FileEventListener.java
+++ src/main/java/com/example/pizzeria/event_listeners/FileEventListener.java
@@ -2,15 +2,19 @@ package com.example.pizzeria.event_listeners;
 
 import com.example.pizzeria.events.ServiceOrderUpdateEvent;
 import com.example.pizzeria.events.UpdateEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class FileEventListener implements UpdateEventListener {
-
     @Override
     @EventListener
     public void update(UpdateEvent event) {
-
+        // todo: implement once the mappers are done
+        log.info("I caught some event!");
     }
 }
