90abe0041b17e105b5c666aa6136ae8fb572c226
styanr <chaus.oleh@gmail.com>
2023-12-10 17:39:32 +0200

    [feat] make event DTOs records; implement file listener

--- src/main/java/com/example/pizzeria/dto/PauseCookDto.java
+++ src/main/java/com/example/pizzeria/dto/PauseCookDto.java
@@ -1,19 +1,6 @@
 package com.example.pizzeria.dto;
 
 import com.example.pizzeria.models.cook.CookStatus;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-public class PauseCookDto {
-
-        private int cookId;
-        private CookStatus cookStatus;
-
-
-        public PauseCookDto(int cookId, CookStatus cookStatus) {
-                this.cookId = cookId;
-                this.cookStatus = cookStatus;
-        }
+public record PauseCookDto(int cookId, CookStatus cookStatus) {
 }
--- src/main/java/com/example/pizzeria/utilities/LoggerConfigUtility.java
+++ src/main/java/com/example/pizzeria/utilities/LoggerConfigUtility.java
@@ -2,7 +2,7 @@ package com.example.pizzeria.utilities;
 
 public class LoggerConfigUtility {
     public static void setNewLogFilename() {
-        String timestamp = java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss:SSS"));
+        String timestamp = java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd_HH_mm_ss_SSS"));
         System.setProperty("logFilename", "logs/simulation_" + timestamp + ".log");
         org.apache.logging.log4j.core.LoggerContext ctx =
                 (org.apache.logging.log4j.core.LoggerContext) org.apache.logging.log4j.LogManager.getContext(false);
