a8788e363d2422b533fdfb57f2a6fcedd902f429
styanr <chaus.oleh@gmail.com>
2023-12-10 18:01:29 +0200

    [fix] add extra newlines

--- src/main/java/com/example/pizzeria/dto/PauseCookDto.java
+++ src/main/java/com/example/pizzeria/dto/PauseCookDto.java
@@ -2,5 +2,4 @@ package com.example.pizzeria.dto;
 
 import com.example.pizzeria.models.cook.CookStatus;
 
-public record PauseCookDto(int cookId, CookStatus cookStatus) {
-}
+public record PauseCookDto(int cookId, CookStatus cookStatus) {}
